17867f128bf23a65ba33c09bdd6f78e2c0fdcc08
Atikur Rahman <fulldevatikur@gmail.com>
2026-05-27 22:46:05 +0600

    fix: update import paths for components to match directory casing in AuthPage, CreateWorkspacePage, HomePage, and OverviewPage

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -3,9 +3,9 @@ import { AppContext } from "../context/AppContext";
 import { useNavigate } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 
-import AuthHeader from "../components/auth/AuthHeader";
-import AuthMain from "../components/auth/AuthMain";
-import AuthFooter from "../components/auth/AuthFooter";
+import AuthHeader from "../components/Auth/AuthHeader";
+import AuthMain from "../components/Auth/AuthMain";
+import AuthFooter from "../components/Auth/AuthFooter";
 
 export default function AuthPage({ IsSignIn }) {
   // 🔹 useContext context
